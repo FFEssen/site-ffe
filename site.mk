b9d466a46122a4419f541ab691d916f7f0ab2e3f
do9xe <h.l.lueth@gmail.com>
2015-06-03 19:35:43 +0200

    changes for version 0.7

--- site.mk
+++ site.mk
@@ -3,8 +3,10 @@ GLUON_SITE_PACKAGES := \
         gluon-alfred \
         gluon-announced \
         gluon-autoupdater \
-        gluon-config-mode-hostname \
+        gluon-setup-mode \
+        gluon-config-mode-core \
         gluon-config-mode-autoupdater \
+        gluon-config-mode-hostname \
         gluon-config-mode-mesh-vpn \
         gluon-config-mode-geo-location \
         gluon-config-mode-contact-info \
@@ -12,8 +14,9 @@ GLUON_SITE_PACKAGES := \
         gluon-ebtables-filter-ra-dhcp \
         gluon-luci-admin \
         gluon-luci-autoupdater \
-        gluon-luci-private-wifi \
         gluon-luci-portconfig \
+        gluon-luci-private-wifi \
+        gluon-luci-wifi-config \
         gluon-next-node \
         gluon-mesh-vpn-fastd \
         gluon-radvd \
@@ -22,10 +25,16 @@ GLUON_SITE_PACKAGES := \
         iptables \
         haveged
 
-
-DEFAULT_GLUON_RELEASE := 0.6.1
+DEFAULT_GLUON_RELEASE := 0.7
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
-GLUON_PRIORITY ?= 0
+GLUON_PRIORITY ?= 10
+GLUON_BRANCH ?= stable
+export GLUON_BRANCH
+
+GLUON_TARGET ?= ar71xx-generic
+export GLUON_TARGET
+
+GLUON_LANGS ?= en de
