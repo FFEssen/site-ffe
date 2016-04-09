55af91ac18b195b4e7140e77b8332a4aebe0fc06
Philip Berndroth <p.berndroth@philuweb.de>
2016-04-09 20:55:45 +0200

    update gluon release

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
         iptables \
         haveged
 
-DEFAULT_GLUON_RELEASE := 0.7.2
+DEFAULT_GLUON_RELEASE := 0.8.3
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
