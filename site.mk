2f0f5f99216024c0db14fc1eac90b6c6a2d28040
Vincent Wiemann <webmaster@codefetch.de>
2019-03-14 18:14:21 +0100

    Update for 4.2-0 (v2018.2)
    Includes update of translation files, ffho-packages for
    WiFi-autoupdater-fallback and an AP timer.
    This version should be much more stable as it has less memory
    consumption than v2017.x.
    Also changed channel and BSSID settings back as we have not yet
    switched to a firmware where all nodes have the autoupdater-
    fallback. Next step after the network is stable again must be to
    introduce domain-specific settings and use the domain-switcher
    package to split the network.

--- site.mk
+++ site.mk
@@ -1,57 +1,128 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-respondd \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location \
+	gluon-config-mode-geo-location-osm \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-radvd \
+ 	gluon-radv-filterd \
+	gluon-respondd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-status-page-mesh-batman-adv \
 	gluon-web-admin \
-	gluon-web-autoupdater \
 	gluon-web-mesh-vpn-fastd \
 	gluon-web-network \
 	gluon-web-private-wifi \
 	gluon-web-wifi-config \
 	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	micrond \
-	ffe-autoupdater-wifi-fallback \
-	iwinfo \
+	ffho-autoupdater-wifi-fallback \
+	ffho-ap-timer \
+	ffho-web-ap-timer \
+	ffho-web-autoupdater \
+	haveged \
 	iptables \
-	haveged
-	
-	
+	iwinfo
 
-ifeq ($(GLUON_TARGET),x86-64)
-# support the usb stack on x86 devices
-# and add a few common USB NICs
-GLUON_SITE_PACKAGES += \
+# Support the USB stack
+USB_PACKAGES_BASIC := \
 	kmod-usb-core \
-	kmod-usb2 \
-	kmod-usb-hid \
+	kmod-usb2
+
+# FAT32 support for USB
+USB_PACKAGES_STORAGE := \
+	block-mount \
+	kmod-fs-ext4 \
+	kmod-fs-vfat \
+	kmod-usb-storage  \
+	kmod-usb-storage-extras  \
+	blkid  \
+	swap-utils  \
+	kmod-nls-cp1250  \
+	kmod-nls-cp1251  \
+	kmod-nls-cp437  \
+	kmod-nls-cp775  \
+	kmod-nls-cp850  \
+	kmod-nls-cp852  \
+	kmod-nls-cp866  \
+	kmod-nls-iso8859-1  \
+	kmod-nls-iso8859-13  \
+	kmod-nls-iso8859-15  \
+	kmod-nls-iso8859-2  \
+	kmod-nls-koi8r  \
+	kmod-nls-utf8
+
+USB_PACKAGES_NET := \
 	kmod-usb-net \
 	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-eem \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-cdc-mbim \
+	kmod-usb-net-cdc-ncm \
+	kmod-usb-net-cdc-subset \
 	kmod-usb-net-dm9601-ether \
-	kmod-r8169
+	kmod-usb-net-hso \
+	kmod-usb-net-huawei-cdc-ncm \
+	kmod-usb-net-ipheth \
+	kmod-usb-net-kalmia \
+	kmod-usb-net-kaweth \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-qmi-wwan \
+	kmod-usb-net-rndis \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-sierrawireless \
+	kmod-usb-net-smsc95xx \
+	kmod-mii \
+	kmod-nls-base
+
+NIC_PACKAGES_NET := \
+	kmod-sky2 \
+	kmod-r8169 \
+	kmod-forcedeth \
+	kmod-8139too
+
+TOOLS_PACKAGES := \
+	bash \
+	tcpdump \
+	vnstat \
+	iperf \
+	iperf3 \
+	socat \
+	usbutils
+
+ifeq ($(GLUON_TARGET),x86-generic)
+GLUON_SITE_PACKAGES += \
+	kmod-usb-hid \
+	$(NIC_PACKAGES_NET) \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(TOOLS_PACKAGES)
 endif
 
-DEFAULT_GLUON_RELEASE := 3.1.3-2
+ifeq ($(GLUON_TARGET),x86-64)
+GLUON_SITE_PACKAGES += \
+	kmod-usb-hid \
+	$(NIC_PACKAGES_NET) \
+	$(USB_PACKAGES_BASIC) \
+	$(USB_PACKAGES_STORAGE) \
+	$(USB_PACKAGES_NET) \
+	$(TOOLS_PACKAGES)
+endif
 
-# Allow overriding the release number from the command line
+DEFAULT_GLUON_RELEASE := 4.2-0
+
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
-
 GLUON_PRIORITY ?= 0
-GLUON_BRANCH ?= beta
-export GLUON_BRANCH
-
-GLUON_TARGET ?= ar71xx-tiny
-export GLUON_TARGET
-
 GLUON_LANGS ?= en de
+GLUON_TARGET ?= ar71xx-tiny
+GLUON_BRANCH ?= beta
